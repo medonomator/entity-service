954d77f4f72b83551be79fe27aae3863a3e61ec3
Diman <budteschastlivi1@gmail.com>
2020-04-19 06:45:36 +0300

    update data

--- static/js/common.js
+++ static/js/common.js
@@ -1,1 +1,47 @@
-// alert(123);
+// Сперва раскрасить по дефолту ячейки
+
+const tableEntitys = document.querySelector('.table-entitys');
+
+setInterval(() => {
+  fetch('http://localhost:5000/entitys/getAll')
+    .then((res) => Promise.resolve(res.json()))
+    .then((res) => {
+      let replaceHtml = '';
+      Object.keys(res).forEach((entity) => {
+        replaceHtml += `<div class="table-column">`;
+        replaceHtml += `<div class="table-row table-row-name">${entity}</div>`;
+        Object.keys(res[entity]).forEach((parameter) => {
+          replaceHtml += `<div class="table-row">${res[entity][parameter]}</div>`;
+        });
+        replaceHtml += `</div>`;
+      });
+
+      replaceHtml += `<div class="table-column">
+                        <div class="table-row table-row-name">Итого:</div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                        <div class="table-row"></div>
+                      </div>`;
+
+      tableEntitys.innerHTML = replaceHtml;
+    });
+}, 1000);
+
+// общий подсчет по параметрам
